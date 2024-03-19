907f33c834803ee58e06c418f34b40b5b7007d0a
Imaniephraim <160492164+Imaniephraim@users.noreply.github.com>
2024-03-19 17:18:49 +0300

    changes

--- src/main/java/org/example/ATMApp.java
+++ src/main/java/org/example/ATMApp.java
@@ -1,4 +1,73 @@
 package org.example;
 
-public class ATMApp {
+import javax.swing.*;
+import java.awt.*;
+
+public class ATMApp extends JFrame {
+    private JPanel mainPanel;
+    private LoginPanel loginPanel;
+    private DashboardPanel dashboardPanel;
+    private RegistrationPanel registrationPanel;
+    private User user;
+
+    public ATMApp() {
+        setTitle("CPL Bank ATM");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        mainPanel = new JPanel(new CardLayout());
+        loginPanel = new LoginPanel(this);
+        registrationPanel = new RegistrationPanel();
+
+        mainPanel.add(loginPanel, "login");
+        // mainPanel.add(registrationPanel, "Registration");
+
+        add(mainPanel, BorderLayout.CENTER);
+        showPanel("Login");
+    }
+
+    public void showPanel(String panelName) {
+        CardLayout cardLayout = (CardLayout) mainPanel.getLayout();
+        cardLayout.show(mainPanel, panelName);
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+
+        if (user != null) {
+            if (dashboardPanel != null) {
+                mainPanel.remove(dashboardPanel);
+            }
+
+            dashboardPanel = new DashboardPanel();
+            mainPanel.add(dashboardPanel, "Dashboard");
+            showPanel("Dashboard");
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/org/example/LoginPanel.java
+++ src/main/java/org/example/LoginPanel.java
@@ -1,4 +1,117 @@
 package org.example;
 
-public class LoginPanel {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+public class LoginPanel extends JPanel {
+
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+
+    private ATMApp app;
+
+    public LoginPanel (ATMApp APP) {
+        this.app = app;
+        setLayout(new GridBagLayout());
+
+        //set background color
+        setBackground(new Color(51, 102, 255));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        //add logo image
+        JLabel logoLabel = new JLabel(new ImageIcon("logo.png"));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        add(logoLabel, gbc);
+
+        JLabel usernameLabel = new JLabel(("Enter Username:"));
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        add(usernameLabel, gbc);
+
+        usernameField = new JTextField(20);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        add(usernameField, gbc);
+
+        JLabel passwordLabel = new JLabel("Enter Password:");
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        add(passwordLabel, gbc);
+
+        passwordField = new JPasswordField(20);
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        add(passwordField, gbc);
+
+        JButton loginButton = new JButton("login");
+        loginButton.addActionListener(new L0ginButtonListener());
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        add(loginButton, gbc);
+
+        JButton registerButton = new JButton("Register");
+        registerButton.addActionListener(new RegisterButtonListerner());
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        add(registerButton, gbc);
+    }
+        private class LoginButtonListener implements ActionListener{
+            @Override
+            public void actionPerformed(ActionEvent e){
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                try {
+                    User user = DatabaseHelper.getUserByUsername(username);
+                    if (user != null && user.getPassword().equals(password)) {
+                        app.setUser();
+                        app.showPanel("Dashboard");
+                    }else {
+                        JOptionPane.showMessageDialog(LoginPanel.this, "Invalid Username or Password!", " Login Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }catch (SQLException ex){
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(LoginPanel.this, "Error Logging In", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+
+        }
+        private class RegisterButtonListener implements ActionListener {
+        @Override
+            public void actionPerformed(ActionEvent e){
+            app.throwPanel("Registration");
+        }
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
